279dabbc63654b80f30129fbe8c492e72d4caad7
enghirushan <eng.hirushan@gmail.com>
2024-03-03 21:55:30 +0530

    add some variable and display

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const a = 10;
+  const firstName="Haritha";
+  const MiddleName = "Hirushan";
+  const  LastName = "ilangasinghe";
+  const age = "22";
+  const country = "Sri lanka";
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React By Haritha ilangasinghe </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1 className="heading">React by Haritha ilangasinghe</h1>
+   
+   <p> {20+20}% for jsx</p>
+   <p>{a}</p>
+   <p>{a>50 ? "greater " :"lower" }</p>
+
+   <br></br>
+   <br></br>
+   
+      <h2>My details</h2>
+    <p> Full name: {firstName} {MiddleName} {LastName}</p>
+    <p>Age : {age}</p>
+    <p>country: {country}</p>
+
     </>
   )
 }
